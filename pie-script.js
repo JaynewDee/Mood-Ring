69539009ed493a62f3b202d71a448b7beee91303
NuminousBlue <jdiehl2236@gmail.com>
2021-09-18 12:25:51 -0500

    Ring colors and class names

--- pie-script.js
+++ pie-script.js
@@ -1,45 +1,51 @@
-$(document).ready(function() {
-     var previous = document.getElementById("menu8"); 
-     var original = "menu";
-     var toOpen = "";
-     var over;
-   
-     var delay = function (elem, callback) { 
-       var timeout = null;
-       timeout = setTimeout(function() { 
-         if(over) {
-           callback(elem);
-         }
-       }, 150);
-   
-       $(elem).mouseout(function() {
-         over = false; 
-         clearTimeout(timeout); 
-       });
-     };
-   
-     $(".over").mouseenter(function(event) { 
-       over = true; 
-       delay(document.getElementById(event.target.id), function(context) {
-         toOpen = document.getElementById(original.concat(context.id));
-         openMenu(toOpen); 
-         previous = toOpen;
-       });
-     });
-   
-     $(".clicky").click(function() {
-       openMenu(document.getElementById("menu8"));
-       previous = document.getElementById("menu8");
-     });
-   
-     function openMenu(context) { 
-       closeMenu(previous);
-       $(context).toggleClass("visible"); 
-       $(context).toggleClass("hidden");
-     }
-   
-     function closeMenu(context) {
-       $(context).toggleClass("visible");
-       $(context).toggleClass("hidden");
-     }
-   });
+$(document).ready(function () {
+  var previous = document.getElementById("menu8");
+  var original = "menu";
+  var toOpen = "";
+  var float;
+  var pageColor = $("#page");
+  var option = $(".option");
+
+  var delay = function (elem, callback) {
+    var timeout = null;
+    timeout = setTimeout(function () {
+      if (float) {
+        callback(elem);
+      }
+    }, 150);
+
+    $(elem).mouseout(function () {
+      float = false;
+      clearTimeout(timeout);
+    });
+  };
+
+  $(".over").mouseenter(function (event) {
+    float = true;
+    delay(document.getElementById(event.target.id), function (context) {
+      toOpen = document.getElementById(original.concat(context.id));
+      openMenu(toOpen);
+      previous = toOpen;
+    });
+  });
+
+  $(".clicky").click(function () {
+    openMenu(document.getElementById("menu8"));
+    previous = document.getElementById("menu8");
+  });
+
+  option.click(function (event) {
+      pageColor.setAttribute("style", "background-color: #D0D0D0");
+  });
+
+  //  function openMenu(context) { 
+  //    closeMenu(previous);
+  //    $(context).toggleClass("visible"); 
+  //    $(context).toggleClass("hidden");
+  //  }
+
+  //  function closeMenu(context) {
+  //    $(context).toggleClass("visible");
+  //    $(context).toggleClass("hidden");
+  //  }
+});
