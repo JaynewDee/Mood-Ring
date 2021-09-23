a730829b657f2290bc66128bf272367118afda55
Val Brisendine <vsbrisendine@gmail.com>
2021-09-22 22:37:20 -0500

    data click events added

--- pie-script.js
+++ pie-script.js
@@ -7,42 +7,30 @@ $(document).ready(function () {
   var insideColor = $('#circle-middle')
   var option = $(".option");
   console.log(pageColor)
-  var searchTerm
+  var searchTerm;
 
+  
 // color variables 
   
 
 //   API key for GIPHY ================================================================================================================
    gifApi = "etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g";
+   var grey = document.querySelector('#meh');
+   var circle = document.getElementsByClassName('circle');
+   var greyData = grey.dataset.parent;
+   // var colorData = button.value;
+   var red = document.querySelector('#lovey-dovey');
+   var orange = document.querySelector('#enthusiasm');
+   var yellow = document.querySelector('#energetic');
+   var green = document.querySelector('#envious');
+   var blue = document.querySelector('#relaxing');
+   var purple = document.querySelector('#rich');
+
+   var feedbackArea = $("#feelings-feedback");
 //  SEARCH ::
 //  https://api.giphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=&limit=25&offset=0&rating=g&lang=en
-//  TRANSLATE : 
-//  https://api.giphy.com/v1/gifs/translate?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&s=
 
 
-let giphyAPI = 'https:/searchTermiphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=&limit=25&offset=0&rating=g&lang=en';
-
-		function giphyGen (){
-
-			fetch(giphyAPI)
-			.then(response => {
-				return response.json();
-			})
-			.then(json => {
-				createImg(json.data[0].images.original.url);
-			})
-      
-			.catch(err => console.log(err));
-
-		}
-// promise.all - look up 
-showGiphyData = (giphy) => {
-    document.getElementById('feels').innerText = 
-    `${giphy.results[0].name.title}`
-}
-fetchedData = (apiData) => {
-  console.log(apiData)
-}
 // FETCH METHOD----------------------------------- GIPHY *********************************************** add class - mood_color.onclick ${this} 
 // to determine where it is on the dom and to have one function 
 option.onclick = () => {
@@ -82,13 +70,7 @@ const apiData = () => {
 fetchedData = (apiData) => {
   console.log(apiData)
 }
-var grey = document.querySelector('#meh');
-var red = document.querySelector('#lovey-dovey');
-var orange = document.querySelector('#enthusiasm');
-var yellow = document.querySelector('#energetic');
-var green = document.querySelector('#envious');
-var blue = document.querySelector('#relaxing');
-var purple = document.querySelector('#rich');
+
 
 console.log(yellow);
 
@@ -122,11 +104,45 @@ console.log(yellow);
 //   COLOR CHANGER -----------------------------------------------------------------------------------
 option.click(function (event) {
     console.log(pageColor.attr("class"))
-    // insideColor.removeClass(pageColor.attr("class")).attr("class", $(this).attr("value"));
+    insideColor.removeClass(pageColor.attr("class")).attr("class", $(this).attr("value"));
     pageColor.removeClass(pageColor.attr("class")).attr("class", $(this).attr("value"));
 
+    // fetch data
+    getGiphy(event.target);
 
   });
+
+    function getGiphy(el) {
+        console.log('click element', el);
+        var moodData = $(el).parent().data('parent');
+        if(moodData) {
+            let giphyAPI = 'https://api.giphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=' + moodData + '&limit=1&lang=en';
+            // console.log(colorData);
+            console.log('API URL', giphyAPI);
+    
+            fetch(giphyAPI)
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                var imageUrl = json.data[0].images.original.url;
+                console.log('IMAGE FROM GIPHY:', imageUrl);
+                feedbackArea.text('').append($('<img src="' + imageUrl + '">'));
+                // createImg();
+            })
+            .catch(err => console.log(err));
+        }
+        
+        // promise.all - look up    
+        // showGiphyData = (giphy) => {
+            // document.getElementById('meh').innerText = 
+            // `${giphy.results[0].name.title}`;
+        //   console.log(document.getElementById('meh').innerText);
+        // }
+        // fetchedData = (apiData) => {
+        //   console.log(apiData)
+        // }
+    }
 
 
 
