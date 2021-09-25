28d14e13cf605d79dfc28237095f9306d476bcc2
Val Brisendine <vsbrisendine@gmail.com>
2021-09-24 20:15:40 -0500

    full active
    fir commit -m :full active

--- pie-script.js
+++ pie-script.js
@@ -1,24 +1,19 @@
 $(document).ready(function () {
-  var previous = document.getElementById("menu8");
-  var original = "menu";
-  var toOpen = "";
+
   var float;
   var pageColor = $('#page');
   var insideColor = $('#circle-middle')
   var option = $(".option");
   console.log(pageColor)
-  var searchTerm;
+  var musicPlayer = $("#musicPlayer")
 
   
-// color variables 
-  
-
+// color variables   
 //   API key for GIPHY ================================================================================================================
    gifApi = "etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g";
-   var grey = document.querySelector('#meh');
+   
    var circle = document.getElementsByClassName('circle');
-   var greyData = grey.dataset.parent;
-   // var colorData = button.value;
+   var grey = document.querySelector('#meh');
    var red = document.querySelector('#lovey-dovey');
    var orange = document.querySelector('#enthusiasm');
    var yellow = document.querySelector('#energetic');
@@ -27,28 +22,15 @@ $(document).ready(function () {
    var purple = document.querySelector('#rich');
 
    var feedbackArea = $("#feelings-feedback");
-//  SEARCH ::
-//  https://api.giphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=&limit=25&offset=0&rating=g&lang=en
 
+// option ON CLICK METHOD----------------------------------- GIPHY 
 
-// FETCH METHOD----------------------------------- GIPHY *********************************************** add class - mood_color.onclick ${this} 
-// to determine where it is on the dom and to have one function 
 option.onclick = () => {
-    giphyGen(feels) //pass in giphyGen - data attribute for each feels value - add api call
+    giphyGen(feels) 
 }
 // =========================================================================================================================================
 
-//STRUCTURE - for ref
-const apiData = () => {
-    fetch(API_URL)
-      .then((res) => {
-        return res.json()
-    }).then((data) => {
-        fetchedData(data)
-    })
-}
-
-
+// --COLOR DISPLAY
   var delay = function (elem, callback) {
     var timeout = null;
     timeout = setTimeout(function () {
@@ -72,40 +54,12 @@ fetchedData = (apiData) => {
 }
 
 
-console.log(yellow);
-
-
-// const giphyGe = (searchTerm) => {
-    // fetch('https:/searchTermiphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=&limit=25&offset=0&rating=g&lang=en')
-    //   .then((res) => {
-        // return res.json()
-    // }).then((data) => {
-        // fetchedData(data)
-    // })
-// }
-
-// $(".over").mouseenter(function (event) {
-  //   float = true;
-  //   delay(document.getElementById(event.target.id), function (context) {
-  //     toOpen = document.getElementById(original.concat(context.id));
-  //     openMenu(toOpen);
-  //     previous = toOpen;
-  //   });
-  // });
-
-  // $(".clicky").click(function () {
-  //   openMenu(document.getElementById("menu8"));
-  //   previous = document.getElementById("menu8");
-  // });
-
-
-
-
 //   COLOR CHANGER -----------------------------------------------------------------------------------
 option.click(function (event) {
     console.log(pageColor.attr("class"))
     insideColor.removeClass(pageColor.attr("class")).attr("class", $(this).attr("value"));
     pageColor.removeClass(pageColor.attr("class")).attr("class", $(this).attr("value"));
+    musicPlayer.removeClass(musicPlayer.attr("src")).attr("src", $(this).attr("data"));
 
     // fetch data
     getGiphy(event.target);
@@ -115,8 +69,9 @@ option.click(function (event) {
     function getGiphy(el) {
         console.log('click element', el);
         var moodData = $(el).parent().data('parent');
+        {
         if(moodData) {
-            let giphyAPI = 'https://api.giphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=' + moodData + '&limit=1&lang=en';
+            let giphyAPI = 'https://api.giphy.com/v1/gifs/search?api_key=etR9QZS3cGJt8xC6rDCi9TJ9ro9Wh08g&q=' + moodData + '&limit=2&lang=en';
             // console.log(colorData);
             console.log('API URL', giphyAPI);
     
@@ -132,35 +87,10 @@ option.click(function (event) {
             })
             .catch(err => console.log(err));
         }
-        
-        // promise.all - look up    
-        // showGiphyData = (giphy) => {
-            // document.getElementById('meh').innerText = 
-            // `${giphy.results[0].name.title}`;
-        //   console.log(document.getElementById('meh').innerText);
-        // }
-        // fetchedData = (apiData) => {
-        //   console.log(apiData)
-        // }
+    }
+    
     }
 
 
-
-
-
-
-
-
-  //  function openMenu(context) { 
-  //    closeMenu(previous);
-  //    $(context).toggleClass("visible"); 
-  //    $(context).toggleClass("hidden");
-  //  }
-
-  //  function closeMenu(context) {
-  //    $(context).toggleClass("visible");
-  //    $(context).toggleClass("hidden");
-  //  }
 });
 
-// fetch('')
